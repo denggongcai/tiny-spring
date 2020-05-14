188d6cce9fafc87b5dc38dd2840dc39c8fa314d8
d.g.c <na@macmini.com>
2020-05-14 22:51:59 +0800

    fix 9-post bean proccessor test

--- 9-auto-create-aop-proxy/src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
+++ 9-auto-create-aop-proxy/src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
@@ -1,12 +1,17 @@
 package us.codecraft.tinyioc;
 
+import org.junit.Assert;
+
 /**
  * @author yihua.huang@dianping.com
  */
 public class OutputServiceImpl implements OutputService {
 
+    private HelloWorldService helloWorldService;
+
     @Override
     public void output(String text){
+        Assert.assertNotNull(helloWorldService);
         System.out.println(text);
     }
 
